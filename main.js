55fa2486ae3383dea4046ed5ac81395176005f00
thommyDD <thommy-mail@t-online.de>
2023-06-10 13:30:58 +0200

    Intervallfunktion für zyklische Abfrage und Datenübermittlung ergänzt

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let solarData = {
     "powerLoad": 0
 }
 
-if (!config.fronius || !config.ccu) return console.error(`Keine Konfigurationsdatei gefunden!\nAbbruch!`)
+if (!config.fronius || !config.ccu) return console.error(`Keine Konfigurationsdatei gefunden oder Konfiguration unvollständig!\nAbbruch!`)
 
 const optionsUrls = {
     optionsUrlPV: {
@@ -24,30 +24,33 @@ const optionsUrls = {
     },
 }
 
-axois.get(config.fronius.urls.flowRealtimeData, optionsUrls)
-.then(data => {
-    
-    let solarDataTmp = data.data.Body;
+console.log("Start zyklische Abfrage der Solardaten");
 
-    solarData.powerAccu = solarDataTmp.Data.Site.P_Akku || 0;
-    solarData.powerGrid = solarDataTmp.Data.Site.P_Grid || 0;
-    solarData.powerLoad = solarDataTmp.Data.Site.P_Load || 0;
-    solarData.powerPv = solarDataTmp.Data.Site.P_PV || 0;
-    solarData.soc = solarDataTmp.Data.Inverters[1].SOC || 0;
+let getSolarData = await axois.get(config.fronius.urls.flowRealtimeData, optionsUrls)
+    .then(data => {
+        
+        let solarDataTmp = data.data.Body;
 
-    // console.log(solarData);
-})
-.then(fetchData => {
-    console.log(fetchData);
+        solarData.powerAccu = solarDataTmp.Data.Site.P_Akku || 0;
+        solarData.powerGrid = solarDataTmp.Data.Site.P_Grid || 0;
+        solarData.powerLoad = solarDataTmp.Data.Site.P_Load || 0;
+        solarData.powerPv = solarDataTmp.Data.Site.P_PV || 0;
+        solarData.soc = solarDataTmp.Data.Inverters[1].SOC || 0;
 
-    const url = `http://${config.ccu.ip}:${config.ccu.port}/sysvar/${config.ccu.sysvars['SoC PV-Speicher']}/~pv`;
-
-    axois.put(url, {"v": solarData.soc})
-    .then(() => {
-        console.log(`Daten an die CCU übermittelt`);
+        // console.log(solarData);
     })
-    .catch(e => console.error(e));
+    .then(fetchData => {
+
+        const url = `http://${config.ccu.ip}:${config.ccu.port}/sysvar/${config.ccu.sysvars['SoC PV-Speicher']}/~pv`;
+
+        axois.put(url, {"v": solarData.soc})
+        .then(() => {
+            console.log(`Daten an die CCU übermittelt`);
+        })
+        .catch(e => console.error(`Es ist ein Fehler bei der Übertragung zur CCU aufgetreten: ${e}`));
+    })
+    .catch(e => {
+        console.error(`Es ist ein Fehler bei Abfrage der Solardaten aufgetreten: ${e}`);
 })
-.catch(e => {
-    console.error(e)
-})
+
+setInterval(getSolarData(), config.requestIntervall * 1_000);
